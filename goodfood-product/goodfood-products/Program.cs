76d7c62633fc0a1b5be46902e122a2594143965b
Valentin BEAURY <vbeaury@sqli.com>
2022-10-17 15:42:10 +0200

    add pipeline + azure db

--- goodfood-product/goodfood-products/Program.cs
+++ goodfood-product/goodfood-products/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ProductContext>(opt =>
-    opt.UseInMemoryDatabase("Products"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("goodfood-db")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();

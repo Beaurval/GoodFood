afd245022155a7602014890d6a33e1d23d8c3e04
Valentin BEAURY <vbeaury@sqli.com>
2022-10-20 21:22:34 +0200

    Add migration user id

--- goodfood-orders/goodfood-orders/Migrations/20221020192212_UserId.Designer.cs
+++ goodfood-orders/goodfood-orders/Migrations/20221020192212_UserId.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using goodfood_orders.Entities;
 
@@ -10,9 +11,10 @@ using goodfood_orders.Entities;
 namespace goodfood_orders.Migrations
 {
     [DbContext(typeof(OrderContext))]
-    partial class OrderContextModelSnapshot : ModelSnapshot
+    [Migration("20221020192212_UserId")]
+    partial class UserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
